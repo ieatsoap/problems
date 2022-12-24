d73fe3b3d525586661e3364311acff9b73c2c4a4
ieatsoap <48131592+ieatsoap@users.noreply.github.com>
2022-12-24 12:13:43 -0600

    https://vjudge.net/problem/CodeForces-731A
    Input is the string to be processed.
    
    Output is the printed int result.

--- NightAtTheMuseum.py
+++ NightAtTheMuseum.py
@@ -1,7 +1,7 @@
 alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 at = 0
 counter = 0
-word = input("gimme a word: ")
+word = input("string : ")
 for i in word:
     if (abs(alph.index(i) - at) < 13):
         counter += abs(alph.index(i)-at)
